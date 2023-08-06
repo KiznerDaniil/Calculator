895b5cc995e131243ecdb0b7793ff8e3bfbf67c5
Даниил <kiznerdaniil2002@gmail.com>
2023-08-07 04:33:28 +0800

    without-loop

--- Calculator.go
+++ Calculator.go
@@ -10,20 +10,36 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-
+	var seq = "1"
 	fmt.Println("Введите выражение:")
-	seq, _ := reader.ReadString('\n') // ВХОДЯЩИЕ ДАННЫЕ
-	var mass []string                 // МАССИВ ДАННЫХ
-	mass = strings.Split(seq, " ")
-	a, _ := strconv.Atoi(mass[0])
-	b := uint(mass[1])
-	fmt.Println(mass, a, b)
-	operation(a, b)
+	seq, _ = reader.ReadString('\n')
+	for {
+		if seq == "0" {
+			fmt.Println("Спасибо за использование данного калькулятора, до свидания!")
+		} else {
+			seq = strings.TrimSpace(seq)
+			var mass []string // МАССИВ ДАННЫХ
+			mass = strings.Split(seq, " ")
+			a, _ := strconv.Atoi(mass[0])
+			b, _ := strconv.Atoi(mass[2])
+			c := mass[1]
+			fmt.Println(operation(a, b, c))
+			fmt.Println("Введите выражение:")
+			seq, _ = reader.ReadString('\n')
+		}
+	}
 }
 
-func operation(X, Y int) int {
-	var c int
-	c = X + Y
-	fmt.Println(X + Y)
-	return c
+func operation(x, y int, z string) (itog int) {
+	switch z {
+	case "+":
+		return x + y
+	case "-":
+		itog = x - y
+	case "*":
+		itog = x * y
+	case "/":
+		itog = x / y
+	}
+	return
 }
