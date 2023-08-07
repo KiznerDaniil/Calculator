46f6d959e21694f5988e11d57a2da4fdefc4cf4d
Даниил <kiznerdaniil2002@gmail.com>
2023-08-07 10:25:15 +0800

    loops

--- Calculator.go
+++ Calculator.go
@@ -9,16 +9,28 @@ import (
 	"strings"
 )
 
-type NumRim[string]int
-
-type Numbers_arab struct {
-	num1 int
-	num2 int
+var RimNum map[string]int = map[string]int{
+	"I" : 1,
+	"II" : 2,
+	"III" : 3,
+	"IV" : 4,
+	"V" : 5,
+	"VI" : 6,
+	"VII" : 7,
+	"VIII" : 8,
+	"IX" : 9,
+	"X" : 10,
+	"L" : 50,
+	"C" : 100,
 }
 
-type Numbers_arab struct {
+type Rim struct {
 	num1 string
 	num2 string
+}
+
+type Arab struct {
+	num1 int
 }
 
 type I interface {
@@ -26,8 +38,46 @@ type I interface {
 	rim_arab() int
 }
 
-func (n Numbers) arab_rim() (A, B string) {
-	m
+func (r Rim) arab_rim(x int) (A string) {
+	RimNum = make(map[string]int)
+	var h int = x / 100 // СОТНИ
+	var t int = x / 10 // ДЕСЯТКИ
+	var ch int = x % t
+	if h == 1 {
+		h1 := "C"
+	} else {
+		switch {
+		case t == 5:
+			t1 := "L"
+		case t > 5:
+			for k, v := range RimNum {
+				if v == t - 5 {
+					t1 = "L" + k
+				}
+			}
+		case t < 5:
+			for k, v := range RimNum {
+				if v == t {
+					t1 = "L" + k
+				}
+			}
+	}
+
+	}
+	for k, v := range RimNum {
+		if v == x {
+
+		}
+	}
+}
+
+func (a Arab) rim_arab(x, y string) (A, B int) {
+	RimNum = make(map[string]int)
+	*a.num1 = x
+	*a.num2 = y
+	A = RimNum[a.num1]
+	B = RimNum[a.num2]
+	return A, B
 }
 
 func main() {
